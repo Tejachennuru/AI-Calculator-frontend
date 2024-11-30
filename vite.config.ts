507abed7f68812786b7b0754db147da5d1de8e3f
Teja Chennuru <tejachennuru05@gmail.com>
2024-11-30 14:15:59 +0530

    Debug commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      alias: {
+          '@': path.resolve(__dirname, './src'), // Alias for the src directory
+      },
   },
-})
+  server: {
+      fs: {
+          allow: ['..'], // Allow importing files outside the root directory
+      },
+  },
+});
